b15c3d015f9eebeb2a746a4eba9f08fbc99f22f3
Vishnu-KV-23 <wishnukv2349@gmail.com>
2025-02-23 18:49:10 +0530

    Initial commit

---
+++ src/test/java/com/vishnudev/Shop/App/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnudev.Shop.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
